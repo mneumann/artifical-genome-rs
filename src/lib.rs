45fedd0c9fe094657204130d7c6ebd36caafd1cc
Michael Neumann <mneumann@ntecs.de>
2016-01-13 17:12:35 +0100

    Implementation of Base trait and DNABase

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+/// Represents the bases used in the genome string.
+/// For example the bases of the DNA are adenine (A),
+/// thymine (T), guanine (G) and cytosine (C).
+trait Base: Sized {
+    /// Returns the "next" base, wrapping around. Used
+    /// to produce the gene product.
+    fn next(self) -> Self;
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum DNABase {
+    A,
+    T,
+    G,
+    C
+}
+
+impl Base for DNABase {
+    fn next(self) -> Self {
+        match self {
+            A => T,
+            T => G,
+            G => C,
+            C => A
+        }
+    }
+}
+
+#[test]
+fn test_dnabase_next() {
+    assert_eq!(DNABase::A, DNABase::C.next());
+}
